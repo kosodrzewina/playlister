26f59d6ab9aa246f07a59a5b9277b2d2d21e7b86
Piotr Sienkiewicz <34711852+kosodrzewina@users.noreply.github.com>
2021-07-12 10:34:59 +0200

    Home page (#4)
    * added flutter svg dependency
    
    * added placeholder logo
    
    * added placeholder avatar
    
    * added border to app bar
    
    * changed background color to black
    
    * changed app bar border
    
    * text color = white
    
    * added recently updated listview
    
    * recently added listview works
    
    * got rid of the floating button
    
    * added elements to recently added listview
    
    * added renderRecentlyUpdatedListElement
    
    * added renderRecentlyAddedListElement
    
    * logo: IconButton -> Container
    
    * added bottom nav bar (does not work just yet)
    
    * set brighteness to dark for the icons in notification bar to be visible
    
    * elements that were being tested do not exist anymore
    
    * renderRecentlyUpdatedListElement -> RecentlyUpdatedListItem
    
    * renderRecentlyAddedListElement -> RecentlyAddedListItem
    
    * including whole folders with assets
    
    * using flutter_gen for assets
    
    * minor changes in code
    
    * /
    
    * themes in a seperate file
    
    * we don't need this
    
    * ButtonStyle -> ElevatedButton.styleFrom()
    
    * Container -> Padding
    
    * ButtonStyle -> ElevatedButton.styleFrom()
    
    * margin -> SizedBox
    
    * not hacky app bar bottom border
    
    * padding in listviews
    
    * RecentlyUpdatedListItem: ElevatedButton -> InkWell
    
    * it almost works
    
    * Container -> SizedBox
    
    * moved list widgets to seperate files
    
    * fixed recently updated list item
    
    * fixed recently added list item
    
    * removed Padding from body
    
    * changed size of UI elements
    
    * themes looking good
    
    * avatar border color changes accordingly to theme
    
    * only one CircleAvatar needed

---
+++ lib/recently_updated_list_item.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class RecentlyUpdatedListItem extends StatelessWidget {
+  final String text;
+  final Color color;
+
+  const RecentlyUpdatedListItem({
+    Key? key,
+    required this.text,
+    required this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 130,
+      decoration: BoxDecoration(
+        color: color,
+        border: Border.all(
+          color: Theme.of(context).hintColor,
+        ),
+        borderRadius: BorderRadius.circular(
+          15,
+        ),
+      ),
+      child: Material(
+        type: MaterialType.transparency,
+        child: InkWell(
+          onTap: () {},
+          borderRadius: BorderRadius.circular(
+            15,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(
+              left: 10,
+              right: 10,
+              bottom: 10,
+            ),
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                text,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
