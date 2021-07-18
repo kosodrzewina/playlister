a4255b02cb896a7a5ab43f8511f568ec3b384ec1
kosodrzewina <sienkiew@live.com>
2021-07-18 22:22:22 +0200

    fields before constructors

--- lib/models.dart
+++ lib/models.dart
@@ -4,6 +4,11 @@ part 'models.g.dart';
 
 @JsonSerializable()
 class YTResponsePlaylistItems {
+  final String kind;
+  final String etag;
+  final String nextPageToken;
+  final List<PlaylistItem> items;
+
   const YTResponsePlaylistItems({
     required this.kind,
     required this.etag,
@@ -11,24 +16,19 @@ class YTResponsePlaylistItems {
     required this.items,
   });
 
-  final String kind;
-  final String etag;
-  final String nextPageToken;
-  final List<PlaylistItem> items;
-
   factory YTResponsePlaylistItems.fromJson(Map<String, dynamic> json) =>
       _$YTResponsePlaylistItemsFromJson(json);
 }
 
 @JsonSerializable()
 class PageInfo {
+  final int totalResults;
+  final int resultsPerPAge;
+
   const PageInfo({
     required this.totalResults,
     required this.resultsPerPAge,
   });
-
-  final int totalResults;
-  final int resultsPerPAge;
 
   factory PageInfo.fromJson(Map<String, dynamic> json) =>
       _$PageInfoFromJson(json);
@@ -36,6 +36,13 @@ class PageInfo {
 
 @JsonSerializable()
 class PlaylistItem {
+  final String kind;
+  final String etag;
+  final String id;
+  final Snippet snippet;
+  final ContentDetails? contentDetails;
+  final Status? status;
+
   const PlaylistItem({
     required this.kind,
     required this.etag,
@@ -45,30 +52,29 @@ class PlaylistItem {
     required this.status,
   });
 
-  final String kind;
-  final String etag;
-  final String id;
-  final Snippet snippet;
-  final ContentDetails? contentDetails;
-  final Status? status;
-
   factory PlaylistItem.fromJson(Map<String, dynamic> json) =>
       _$PlaylistItemFromJson(json);
 }
 
 @JsonSerializable()
 class Status {
+  final String privacyStatus;
+
   const Status({
     required this.privacyStatus,
   });
-
-  final String privacyStatus;
 
   factory Status.fromJson(Map<String, dynamic> json) => _$StatusFromJson(json);
 }
 
 @JsonSerializable()
 class ContentDetails {
+  final String videoId;
+  final String startAt;
+  final String endAt;
+  final String note;
+  final DateTime videoPublishedAt;
+
   const ContentDetails({
     required this.videoId,
     required this.startAt,
@@ -77,18 +83,24 @@ class ContentDetails {
     required this.videoPublishedAt,
   });
 
-  final String videoId;
-  final String startAt;
-  final String endAt;
-  final String note;
-  final DateTime videoPublishedAt;
-
   factory ContentDetails.fromJson(Map<String, dynamic> json) =>
       _$ContentDetailsFromJson(json);
 }
 
 @JsonSerializable()
 class Snippet {
+  final String publishedAt;
+  final String channelId;
+  final String title;
+  final String description;
+  final Thumbnails thumbnails;
+  final String channelTitle;
+  final String playlistId;
+  final int position;
+  final ResourceId resourceId;
+  final String videoOwnerChannelTitle;
+  final String videoOwnerChannelId;
+
   const Snippet({
     required this.publishedAt,
     required this.channelId,
@@ -103,33 +115,21 @@ class Snippet {
     required this.videoOwnerChannelId,
   });
 
-  final String publishedAt;
-  final String channelId;
-  final String title;
-  final String description;
-  final Thumbnails thumbnails;
-  final String channelTitle;
-  final String playlistId;
-  final int position;
-  final ResourceId resourceId;
-  final String videoOwnerChannelTitle;
-  final String videoOwnerChannelId;
-
   factory Snippet.fromJson(Map<String, dynamic> json) =>
       _$SnippetFromJson(json);
 }
 
 @JsonSerializable()
 class Thumbnail {
+  final String url;
+  final int width;
+  final int height;
+
   const Thumbnail({
     required this.url,
     required this.width,
     required this.height,
   });
-
-  final String url;
-  final int width;
-  final int height;
 
   factory Thumbnail.fromJson(Map<String, dynamic> json) =>
       _$ThumbnailFromJson(json);
@@ -137,6 +137,12 @@ class Thumbnail {
 
 @JsonSerializable()
 class Thumbnails {
+  @JsonKey(name: 'default')
+  // ignore: non_constant_identifier_names
+  final Thumbnail default_;
+  final Thumbnail medium;
+  final Thumbnail high;
+
   const Thumbnails({
     // ignore: non_constant_identifier_names
     required this.default_,
@@ -144,25 +150,19 @@ class Thumbnails {
     required this.high,
   });
 
-  @JsonKey(name: 'default')
-  // ignore: non_constant_identifier_names
-  final Thumbnail default_;
-  final Thumbnail medium;
-  final Thumbnail high;
-
   factory Thumbnails.fromJson(Map<String, dynamic> json) =>
       _$ThumbnailsFromJson(json);
 }
 
 @JsonSerializable()
 class ResourceId {
+  final String kind;
+  final String videoId;
+
   const ResourceId({
     required this.kind,
     required this.videoId,
   });
-
-  final String kind;
-  final String videoId;
 
   factory ResourceId.fromJson(Map<String, dynamic> json) =>
       _$ResourceIdFromJson(json);
