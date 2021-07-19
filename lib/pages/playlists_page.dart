8d8cb338639828c63f6147d79322cb9ca9f04150
kosodrzewina <sienkiew@live.com>
2021-07-19 23:38:02 +0200

    if there are playlists, they'll be displayed on playlists page

--- lib/pages/playlists_page.dart
+++ lib/pages/playlists_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../gen/assets.gen.dart';
 import '../l10n/l10n.dart';
+import '../stores/playlist_store.dart';
+import '../widgets/playlists_list_item.dart';
 
 class PlaylistsPage extends StatefulWidget {
   const PlaylistsPage();
@@ -16,23 +19,69 @@ class _PlaylistsPageState extends State<PlaylistsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ListView(
-        shrinkWrap: true,
-        children: [
-          SizedBox(
-            height: 250,
-            width: 250,
-            child: Assets.icons.lookingAtVoid.svg(),
-          ),
-          const SizedBox(height: 10),
-          Center(
-            child: Text(
-              L10n.of(context)!.noPlaylistsFound,
+    if (context.read<PlaylistStore>().playlists == null) {
+      return Center(
+        child: ListView(
+          shrinkWrap: true,
+          children: [
+            SizedBox(
+              height: 250,
+              width: 250,
+              child: Assets.icons.lookingAtVoid.svg(),
             ),
+            const SizedBox(height: 10),
+            Center(
+              child: Text(
+                L10n.of(context)!.noPlaylistsFound,
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(right: 10),
+          child: DropdownButton<String>(
+            value: currentSort,
+            icon: const Icon(Icons.filter_list),
+            items: [
+              for (var item in ['Sort by 0', 'Sort by 1', 'Sort by 2'])
+                DropdownMenuItem(
+                  value: item,
+                  child: Text(item),
+                ),
+            ],
+            onChanged: (value) {
+              setState(() {
+                currentSort = value!;
+              });
+            },
           ),
-        ],
-      ),
+        ),
+        Expanded(
+          child: ListView(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 10,
+            ),
+            shrinkWrap: true,
+            children: [
+              const SizedBox(height: 10),
+              for (var item
+                  in context.read<PlaylistStore>().playlists!.items) ...[
+                PlaylistsListItem(
+                  text: item.snippet!.title,
+                  color: Colors.teal,
+                ),
+                const SizedBox(height: 10),
+              ]
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
