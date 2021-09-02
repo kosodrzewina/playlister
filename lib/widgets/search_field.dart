cfd4476e2008a496772fd5fad3710b7d0ee6d53f
kosodrzewina <sienkiew@live.com>
2021-09-02 14:42:59 +0200

    search field is disabled if there's no API key

--- lib/widgets/search_field.dart
+++ lib/widgets/search_field.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:playlister/stores/auth_store.dart';
+import 'package:provider/provider.dart';
 
 class SearchField extends StatefulWidget {
   final String labelText;
   final Color accentColor;
   final ValueChanged<String> onSubmitted;
+  final bool isEnabled;
 
   const SearchField({
     required this.labelText,
     required this.accentColor,
     required this.onSubmitted,
+    this.isEnabled = true,
   });
 
   @override
@@ -29,34 +34,52 @@ class _SearchFieldState extends State<SearchField> {
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(25),
-          borderSide: BorderSide.none,
+    return Observer(
+      builder: (_) => TextField(
+        enabled: context.read<AuthStore>().apiKey != null,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(25),
+            borderSide: BorderSide.none,
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(25),
+            borderSide: BorderSide.none,
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(25),
+            borderSide: BorderSide.none,
+          ),
+          fillColor: context.read<AuthStore>().apiKey != null
+              ? Theme.of(context).cardColor
+              : Theme.of(context).cardColor.withOpacity(0.3),
+          filled: true,
+          prefixIcon: Icon(
+            Icons.search,
+            color: _isFocused
+                ? context.read<AuthStore>().apiKey != null
+                    ? widget.accentColor
+                    : widget.accentColor.withOpacity(0.3)
+                : context.read<AuthStore>().apiKey != null
+                    ? Theme.of(context).iconTheme.color!
+                    : Theme.of(context).iconTheme.color!.withOpacity(0.3),
+          ),
+          labelStyle: TextStyle(
+            color: context.read<AuthStore>().apiKey != null
+                ? Theme.of(context).textTheme.bodyText1!.color!
+                : Theme.of(context)
+                    .textTheme
+                    .bodyText1!
+                    .color!
+                    .withOpacity(0.3),
+          ),
+          labelText: widget.labelText,
+          floatingLabelBehavior: FloatingLabelBehavior.never,
         ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(25),
-          borderSide: BorderSide.none,
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(25),
-          borderSide: BorderSide.none,
-        ),
-        fillColor: Theme.of(context).cardColor,
-        filled: true,
-        prefixIcon: Icon(
-          Icons.search,
-          color: _isFocused
-              ? widget.accentColor
-              : Theme.of(context).iconTheme.color,
-        ),
-        labelText: widget.labelText,
-        floatingLabelBehavior: FloatingLabelBehavior.never,
+        focusNode: _focusNode,
+        cursorColor: widget.accentColor,
+        onSubmitted: widget.onSubmitted,
       ),
-      focusNode: _focusNode,
-      cursorColor: widget.accentColor,
-      onSubmitted: widget.onSubmitted,
     );
   }
 }
